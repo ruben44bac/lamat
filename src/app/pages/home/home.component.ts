8f8007d245d6e7244558ce8e498c1df95f860d17
mac <mac@MacBook-Pro-de-mac.local>
2019-04-23 20:41:45 -0500

    ajuste nombres e imagenes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -17,118 +17,81 @@ import { Subscription } from 'rxjs';
 export class HomeComponent implements OnInit {
   public productos: Array<item> = [  
     {
-      descripcion: "",
-      nombre: "Atole de cajeta (1 lt)	",
-      precio: 54.00,
-      id: 1
-    },
-    {
-      descripcion: "",
-      nombre: "Atole de guayaba (1 lt)",
-      precio: 54.00,
-      id: 2
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales canarios",
-      precio: 25.00,
-      id: 3
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales chiapanecos ",
-      precio: 25.00,
-      id: 4
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales costeños rojos	",
-      precio: 23.00,
-      id: 5
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales costeños verdes",
-      precio: 23.00,
-      id: 6
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales de cajeta	",
-      precio: 25.00,
-      id: 7
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales de champiñones",
-      precio: 23.00,
-      id: 8
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales de chicharrón",
-      precio: 22.00,
-      id: 9
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales de Dulce	",
-      precio: 22.00,
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/atole-cajeta_5526a64f-ac74-40c6-a07f-102bc430f31c_large.jpg",
+      nombre: "Champurrado (32 onzas)",
+      precio: 7.00,
       id: 10
     },
     {
-      descripcion: "",
-      nombre: "Tamales de guayaba",
-      precio: 22.00,
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/atole-cajeta_5526a64f-ac74-40c6-a07f-102bc430f31c_large.jpg",
+      nombre: "Atole de fresa (32 onzas)",
+      precio: 7.00,
       id: 11
     },
     {
-      descripcion: "",
-      nombre: "Tamales de mole",
-      precio: 22.00,
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/atole-cajeta_5526a64f-ac74-40c6-a07f-102bc430f31c_large.jpg",
+      nombre: "Atole de chocolate (32 onzas)",
+      precio: 7.00,
       id: 12
     },
     {
-      descripcion: "",
-      nombre: "Tamales de piña con nuez",
-      precio: 22.00,
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/atole-cajeta_5526a64f-ac74-40c6-a07f-102bc430f31c_large.jpg",
+      nombre: "Atole de vainilla (32 onzas)",
+      precio: 7.00,
       id: 13
     },
     {
-      descripcion: "",
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/verde_large.jpg",
+      nombre: "Tamales verdes con pollo",
+      precio:  2.25,
+      id: 1
+    },
+    {
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/chicharron_large.jpg",
+      nombre: "Tamales rojos con pollo",
+      precio:  2.25,
+      id: 2
+    },
+    {
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/generico-hoja-maiz_6fa029e4-23a2-4ec5-b4ea-1bb9bfed22d3_large.jpg",
+      nombre: "Tamales de mole con pollo",
+      precio:  2.25,
+      id: 3
+    },
+    {
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/verde_large.jpg",
+      nombre: "Tamales verdes con puerco",
+      precio:  2.25,
+      id: 4
+    },
+    {
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/chicharron_large.jpg",
+      nombre: "Tamales rojos con puerco",
+      precio:  2.25,
+      id: 5
+    },
+    {
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/tamal_de_rajas_con_queso_large.jpg",
       nombre: "Tamales de rajas con queso",
-      precio: 22.00,
-      id: 14
+      precio:  2.25,
+      id: 6
+    },{
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/generico-hoja-maiz_6fa029e4-23a2-4ec5-b4ea-1bb9bfed22d3_large.jpg",
+      nombre: "Tamales de frijoles con chorizo",
+      precio:  2.25,
+      id: 7
     },
     {
-      descripcion: "",
-      nombre: "Tamales oaxaqueños",
-      precio: 23.00,
-      id: 15
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/tamal-de-dulce_large.jpg",
+      nombre: "Tamales de fresa",
+      precio:  2.25,
+      id: 8
     },
     {
-      descripcion: "",
-      nombre: "Tamales poblanos",
-      precio: 23.00,
-      id: 16
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales veracruzanos	",
-      precio: 25.00,
-      id: 17
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales verdes",
-      precio: 22.00,
-      id: 18
-    },
-    {
-      descripcion: "",
-      nombre: "Tamales yucatecos	",
-      precio: 25.00,
-      id: 19
+      descripcion: "https://cdn.shopify.com/s/files/1/0346/5933/products/generico-hoja-maiz_6fa029e4-23a2-4ec5-b4ea-1bb9bfed22d3_large.jpg",
+      nombre: "Tamales de piña",
+      precio:  2.25,
+      id: 9
     }
   ];
   constructor(public dialog: MatDialog, 
